7fe51802245303dc3ef2a62cecaae53ce3d291f3
etgar <etgar.ruttner@gmail.com>
2022-04-22 13:28:33 +0300

    modified scan

--- scan.py
+++ scan.py
@@ -4,9 +4,8 @@ from sensor_msgs.msg import LaserScan
 
 
 def callback(msg):
-    print(f"angle is {0}    --  distance is {66666666666666666666666}.")
     for angle in range(0,len(msg.ranges)-1):
-        print(f"angle is {angle}    --  distance is {msg.ranges[angle]}.")
+        print("angle is {}  --  distance is {}".format(angle, msg.ranges[angle]))
 
 rospy.init_node('scan_values')
 sub = rospy.Subscriber('/scan', LaserScan, callback)
