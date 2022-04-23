4f42219037c3af581a0347d7b3b6eeeece2bc6c3
etgar <etgar.ruttner@gmail.com>
2022-04-23 14:39:59 +0300

    modified run & scan

--- scan.py
+++ scan.py
@@ -5,14 +5,15 @@ from sensor_msgs.msg import LaserScan
 class scanner():
     def __init__(self):
         #rospy.init_node('scan_values')
-        rospy.Subscriber('/scan', LaserScan, self.callback)
+        self.sub = rospy.Subscriber('/scan', LaserScan, self.callback)
         self._scan = LaserScan()
 
     def callback(self,msg):
+        print('im here')
         self._scan=msg
 
     def get_scan_data(self):
-        return self._scan.ranges
+        return self._scan
 
 
 
