dd3202d0695ab3f6ca2c6288d7f6884b3a909fb9
etgar <etgar.ruttner@gmail.com>
2022-05-06 18:02:30 +0300

    modified run - etgar 6.5

--- run.py
+++ run.py
@@ -21,9 +21,11 @@ class RunRobot:
         self.cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
         self.scanner = scanner()
 
-        # just to understand what happened and how to !
+        data = self.scanner.get_scan_data()
+        rospy.loginfo([round(angle, 1) for angle in data[85:95]])
 
-        count = 0
+
+        # count = 0
 
         # while True:
         #     time.sleep(1)
