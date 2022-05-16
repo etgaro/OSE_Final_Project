63e56fb2a4fa1a782366cf05b171208ed7cd0448
OraShumacher <orashum@post.bgu.ac.il>
2022-05-16 18:10:59 +0300

    clockwise

--- run.py
+++ run.py
@@ -22,7 +22,7 @@ class RunRobot:
         self.cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
         self.scanner = scanner()
         rospy.loginfo("i scanned")
-        while(not rospy.on_shutdown):
+        while not rospy.on_shutdown:
             data = self.scanner.get_scan_data()
             rospy.loginfo([round(angle, 1) for angle in data[85:95]])
 
