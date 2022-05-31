d5bae2c33c335d01f00477419c526c70bfb5bc66
etgar <etgar.ruttner@gmail.com>
2022-05-31 18:21:01 +0300

    update scan

--- scan.py
+++ scan.py
@@ -68,6 +68,8 @@ class scanner():
     def get_tree_angles_dist(self):
         data = self.get_scan_data()
         distance_between_trees = 0.7
+
+        rospy.loginfo(type(data))
         for angle in range(0,180,1):
             if data[angle]>distance_between_trees*2:
                 data[angle] = 0
