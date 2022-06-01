f6ab39b0e3e2606d04dac9cef01615a614ab0b80
etgar <etgar.ruttner@gmail.com>
2022-06-01 13:42:20 +0300

    update tree

--- tree_run.py
+++ tree_run.py
@@ -42,8 +42,8 @@ class RunRobot:
 
         self.r = rospy.Rate(10)
 
-        self.keep_from_wall_max = 0.30
-        self.keep_from_wall_min = 0.15
+        self.keep_from_wall_max = 0.40
+        self.keep_from_wall_min = 0.25
 
         self.cmd_vel.publish(self.move_cmd_straight)
         # wait for 0.1 seconds (10 HZ) and publish again
